3e539541683e48d61a29d830e29fd168112c34db
justin <walkerjt93@gmail.com>
2020-02-11 19:48:37 -0500

    learned about injecting services into other services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,24 +1,18 @@
 import { Component, Input } from '@angular/core';
-import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  providers: [LoggingService],
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent {
   @Input() account: { name: string; status: string };
   @Input() id: number;
 
-  constructor(
-    private loggingService: LoggingService,
-    private accountService: AccountsService
-  ) {}
+  constructor(private accountService: AccountsService) {}
 
   onSetTo(status: string) {
     this.accountService.updateStatus(this.id, status);
-    this.loggingService.logStatus(status);
   }
 }
--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AccountsService {
   accounts = [
     {
       name: 'Master Account',
-      status: 'active',
+      status: 'active'
     },
     {
       name: 'Testaccount',
-      status: 'inactive',
+      status: 'inactive'
     },
     {
       name: 'Hidden Account',
-      status: 'unknown',
-    },
+      status: 'unknown'
+    }
   ];
 
-  constructor() {}
+  constructor(private loggingService: LoggingService) {}
 
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
+    this.loggingService.logStatus(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatus(status);
   }
 }
